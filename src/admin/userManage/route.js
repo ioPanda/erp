20dd88c57012b81324aa3636dd43d4e42e05f665
zz0377 <1471900543@qq.com>
2016-08-04 23:38:17 +0800

    add admin

---
+++ src/admin/userManage/route.js
@@ -0,0 +1,11 @@
+import {Route} from "backbone-routing";
+import View from "./view";
+
+export default Route.extend({
+	 initialize(options={}){
+	 	 this.container=options.container;
+
+	 	 this.view=new View();
+	 	 this.container.show(this.view);
+	 }
+});
---
+++ src/admin/userManage/view.js
@@ -0,0 +1,10 @@
+import {ItemView} from "backbone.marionette";
+import template from "./template.hbs";
+
+export default ItewView.extend({
+	template:template,
+	className:"userManage",
+    
+    
+
+});
