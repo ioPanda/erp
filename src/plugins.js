2b2d7e22ca15edd441d12dcbc99aab8ff88ed4b6
方青 <fangqing@fangqingdeMacBook-Air.local>
2016-08-16 13:00:06 +0800

    qing 3th

--- src/plugins.js
+++ src/plugins.js
@@ -12,29 +12,3 @@ import 'babel-polyfill';
 if (window.__agent) {
   window.__agent.start(Backbone, Marionette);
 }
-
-
-//市场管理各类市场className
-Handlebars.registerHelper('marketN',function(marketName) {
-     
-     switch(marketName){
-     	case '亚洲市场' :
-            return aisa;
-	     	break;
-	    case '区域市场' :
-		    return region;
-		    break;
-		case '本地市场' :
-		    return native;
-		    break;
-		case '国内市场' :
-			return domestic;
-			break;
-		case '国际市场' :
-		    return internet;
-		    break;
-		default :
-		    break;
-      }
-
-});
