4f21a8cb2011757076001a84286f88a32e74275b
zz0377 <1471900543@qq.com>
2016-09-16 22:23:45 +0800

    add ajax

--- src/admin/groupManage/layout-view.js
+++ src/admin/groupManage/layout-view.js
@@ -4,6 +4,7 @@ import {LayoutView} from 'backbone.marionette';
 import CollectionView from './content/collection-view';
 import {Collection} from 'backbone';
 import template from './layout-template.hbs';
+import ModalService from '../../component/modal/service';
 
 export default LayoutView.extend({
   template: template,
@@ -61,6 +62,7 @@ export default LayoutView.extend({
   //页面事件绑定部分
   ui: {
      pageLimit : '#pageLimit',
+     page      : '.pagination li a',
      details   : '.glyphicon-plus',
      delete    : '.delete',
      addHistroy: '.addHistroy'
@@ -68,14 +70,15 @@ export default LayoutView.extend({
 
   events: {
     'change @ui.pageLimit': 'changeLimit',
+    'click @ui.page'      : 'changePage',
     'click @ui.details'   : 'toggleDetails',
     'click @ui.delete'    : 'delete',
     'click @ui.addHistroy': 'addHistroy'
   },
 
-  changeLimit(e){
+  changeLimit(e) {
       //重置列表内容
-      this.state.limit=$('.page select').val();
+      this.state.limit = $('.page select').val();
       this.state.start = (this.page - 1) * this.state.limit;
 
       let filtered = _.chain(this.data)
@@ -98,10 +101,10 @@ export default LayoutView.extend({
       //重置页码数
       let num = Math.ceil(this.data.length / this.state.limit);
       
-      if($('.pagination').children().length!=(num+2)){
+      if($('.pagination').children().length != (num+2)){
          $('.pagination').html('');
          $('.pagination').append('<li class="disabled"><a>&laquo;</a></li>');
-         for(let i=0;i<num;i++){
+         for(let i=0; i<num; i++){
             $('.pagination').append('<li><a href="#colors?page='+(i+1)+'">'+(i+1)+'</a></li>');
          }
          $('.pagination').append('<li><a>&raquo;</a></li>');
@@ -113,7 +116,47 @@ export default LayoutView.extend({
 
   },
 
-  toggleDetails(e){
+  changePage(e) {
+    var $$ = $('.pagination').children();
+    if(($(e.target).text() === $$.eq(0).text())&&(!$$.eq(0).hasClass('disabled'))){
+       this.page -= 1;
+    }else if(($(e.target).text() === $$.last().text())&&(!$$.last().hasClass('disabled'))){
+       this.page += 1;
+    }else if(($(e.target).text() === $$.eq(0).text())&&($$.eq(0).hasClass('disabled'))){
+       this.page = 1;
+    }else if(($(e.target).text() === $$.last().text())&&($$.last().hasClass('disabled'))){
+       this.page = $$.length-2;
+    }
+    else{
+      this.page = parseInt($(e.target).text());
+    }
+     
+     this.state.start = (this.page-1) * this.state.limit;
+     
+     let filtered = _.chain(this.data)
+            .drop(this.state.start)
+            .take(this.state.limit)
+            .value();
+
+          this.filteredCollection = new Collection(filtered);
+
+          this.collectionView = new CollectionView({
+             collection: this.filteredCollection
+          });
+
+     this.list.show(this.collectionView);
+
+     $('.group__item').unwrap();   // 去除 <tr class=''> 外部包含的 <tr class="">  -----表格布局
+
+     $('.details').hide();
+      
+     $$.removeClass('active');
+     $$.eq(this.page).addClass('active');
+     this.page === 1 ? $$.eq(0).addClass('disabled') : $$.eq(0).removeClass('disabled');
+     this.page === ($$.length-2) ? $$.last().addClass('disabled') : $$.last().removeClass('disabled');
+  },
+
+  toggleDetails(e) {
     var $this = $(e.target).parent().parent();
     var $next = $this.nextUntil('.item');
 
@@ -126,12 +169,64 @@ export default LayoutView.extend({
     // });
   },
 
-  delete(){
-    alert('delete');
+  delete(e) {
+     let rowData = $(e.target).parent().parent().children();
+     ModalService.request('confirm', {
+       title : '',
+       text: '确定删除'+ rowData.eq(1).html() +'游戏组？删除后将结束游戏！' 
+     }).then(confirmed => {
+       if (!confirmed) {
+         return;
+       } else {
+         let arr = [];    // 保存要删除的  userId
+         let deleteId = rowData.eq(1).html();
+         arr.push(deleteId);
+         
+         let jqXHR = $.ajax({
+             type: 'GET',
+             url: '/userManagerController/getUserList.do',
+             data: arr
+         });
+
+         jqXHR.done(function(response) {
+             alert(arr + ' delete');
+         });
+
+         jqXHR.fail(function(xhr, errorText, errorStatus) {
+             alert('there is a error in delete');
+         });
+       }
+     });     
   },
 
-  addHistroy(){
-    alert('addHistroy');
+  addHistroy(e) {
+    let rowData = $(e.target).parent().parent().children();
+    ModalService.request('confirm', {
+       title : '',
+       text: '确定将'+ rowData.eq(1).html() +'游戏组加入历史记录？' 
+    }).then(confirmed => {
+       if (!confirmed) {
+         return;
+       } else {
+         let arr = [];    
+         let deleteId = rowData.eq(1).html();
+         arr.push(deleteId);
+         
+         let jqXHR = $.ajax({
+             type: 'GET',
+             url: '/userManagerController/getUserList.do',
+             data: arr
+         });
+
+         jqXHR.done(function(response) {
+             alert(arr + ' addHistory');
+         });
+
+         jqXHR.fail(function(xhr, errorText, errorStatus) {
+             alert('there is a error in addHistory');
+         });
+       }
+    });     
   }
 
 });
