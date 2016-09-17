2fade19ec31dd2be8fc59e8569a24bebd19a4ba3
zz0377 <1471900543@qq.com>
2016-09-17 22:33:38 +0800

    D:/git/Git

--- src/admin/userList/collection.js
+++ src/admin/userList/collection.js
@@ -2,7 +2,9 @@ import {Collection} from 'backbone';
 import Model from './model';
 
 export default Collection.extend({
-  url: '/userManagerController/getUserList.do',
+  url: '/erp/userManager/findAllApprovedUser.do',
+
+  // url: '/userManagerController/getUserList.do',
   // url: '/api/colors',
   model: Model
 });
